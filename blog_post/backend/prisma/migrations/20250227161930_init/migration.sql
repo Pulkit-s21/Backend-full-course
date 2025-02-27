08b36274372a156636a988c9a1a75326481ac6b7
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-02-27 22:10:24 +0530

    Minor changes, Register user from .rest not working (USE POSTMAN)

--- blog_post/backend/prisma/migrations/20250227161930_init/migration.sql
+++ blog_post/backend/prisma/migrations/20250227161930_init/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE "Users" (
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "image" TEXT,
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
 );
