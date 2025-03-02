f2d9bca90555d4c8df72641c3c134b76c097131d
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-03-02 13:30:55 +0530

    Delete btn only on profile page

--- blog_post/frontend/src/components/VerticalBlog.jsx
+++ blog_post/frontend/src/components/VerticalBlog.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
-import moment from "moment"
 import { deleteBlog } from "../services/blogServices"
+import { useLocation } from "react-router-dom"
+import moment from "moment"
 import Swal from "sweetalert2"
 
 export const VerticalBlog = ({
@@ -12,6 +13,7 @@ export const VerticalBlog = ({
   tags,
   id,
 }) => {
+  const location = useLocation()
   const token = localStorage.getItem("token")
 
   const removeBlog = async (id) => {
@@ -46,25 +48,27 @@ export const VerticalBlog = ({
           <p className="text-slate-500">{moment(createdAt).fromNow()}</p>
         </div>
 
-        <button
-          onClick={() =>
-            Swal.fire({
-              icon: "warning",
-              title: "Are you sure you want to delete it ?",
-              showCancelButton: true,
-              cancelButtonColor: "orange",
-              confirmButtonText: "Yes",
-              confirmButtonColor: "red",
-            }).then((res) => {
-              if (res.isConfirmed) {
-                removeBlog(id)
-              }
-            })
-          }
-          className="bg-red-400 w-fit px-6 py-1 text-white rounded-2xl cursor-pointer"
-        >
-          Delete
-        </button>
+        {location.pathname === "/profile" && (
+          <button
+            onClick={() =>
+              Swal.fire({
+                icon: "warning",
+                title: "Are you sure you want to delete it ?",
+                showCancelButton: true,
+                cancelButtonColor: "orange",
+                confirmButtonText: "Yes",
+                confirmButtonColor: "red",
+              }).then((res) => {
+                if (res.isConfirmed) {
+                  removeBlog(id)
+                }
+              })
+            }
+            className="bg-red-400 w-fit px-6 py-1 text-white rounded-2xl cursor-pointer"
+          >
+            Delete
+          </button>
+        )}
       </div>
 
       {/* blog info */}
