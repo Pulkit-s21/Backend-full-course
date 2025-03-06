8a3b586062f46a73cbe696e1fb59dc1ab908883d
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-03-06 16:58:40 +0530

    Token expiration check, logout if expired

--- blog_post/frontend/src/helpers/UserContext.jsx
+++ blog_post/frontend/src/helpers/UserContext.jsx
@@ -2,6 +2,7 @@
 import { createContext, useState, useEffect } from "react"
 import { jwtDecode } from "jwt-decode"
 import { getUserDetails } from "../services/authServices"
+import Swal from "sweetalert2"
 
 // Create UserContext
 export const UserContext = createContext(null)
@@ -11,6 +12,17 @@ export const UserProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [loading, setLoading] = useState(true)
 
+  const logoutAlert = () => {
+    Swal.fire({
+      icon: "info",
+      text: "Your session has expired. Please log in again",
+      showConfirmButton: true,
+      confirmButtonText: "Ok",
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+    }).then((res) => res.isConfirmed && logout())
+  }
+
   useEffect(() => {
     const initializeUser = async () => {
       const storedToken = localStorage.getItem("token")
@@ -19,11 +31,20 @@ export const UserProvider = ({ children }) => {
         setLoading(false)
         return
       }
+
       try {
         const decodedUser = jwtDecode(storedToken)
         const userData = await getUserDetails(decodedUser.id, storedToken)
         setUser(userData)
         setIsLoggedIn(true)
+
+        // checking if token is expired or not
+        const timeUntilTokenExpires = decodedUser.exp * 1000 - Date.now()
+        const logoutTimer = setTimeout(() => {
+          logoutAlert()
+        }, timeUntilTokenExpires)
+
+        return () => clearTimeout(logoutTimer)
       } catch (error) {
         console.error("Invalid token", error)
         logout()
